247d9b0d98c1a50e339542fc1c7da42dd3615eb3
LonelySnowman <2438490168@qq.com>
2023-12-05 10:43:17 +0800

    feat: storybook新增调试组件

--- packages/components/button/_story/button.stories.tsx
+++ packages/components/button/_story/button.stories.tsx
@@ -1,16 +1,31 @@
-import type { StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react'
 import Button from '../index';
 
-export default {
+const meta: Meta<typeof Button> = {
     title: 'Button',
-}
+    component: Button,
+};
 
-export const Base: StoryObj = {
-    args: {
-        primary: true,
-        label: 'Button',
+export const Basic: StoryObj = {
+    parameters: {
+        layout: 'centered', // 将组件居中显示
     },
+    args: {
+        children: '按钮',
+        type: 'default'
+    },
+    argTypes: {
+        type: {
+            options: ['default', 'primary', 'warning', 'danger'],
+            control: { type: 'radio' },
+        },
+    }
+};
+
+export default meta;
+
+export const Type: StoryObj = {
     render() {
         return (<>
             <Button>Default</Button>
