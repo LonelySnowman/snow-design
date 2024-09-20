bde2462c04e1c9f6b307dc4bdcf40ba66728351e
LonelySnowman <2438490168@qq.com>
2024-09-21 01:54:45 +0800

    feat: add vue index file

---
+++ packages/vue3/components.ts
@@ -0,0 +1,8 @@
+export type { ButtonProps } from './button';
+export { default as Button } from './button';
+
+export type { ConfigProviderProps } from './config-provider';
+export { default as ConfigProvider } from './config-provider';
+
+export type { paginationProps } from './pagination';
+export { default as Pagination } from './pagination';
---
+++ packages/vue3/index.ts
@@ -0,0 +1,17 @@
+import type { App } from 'vue';
+
+import * as components from './components';
+
+export const install = function (app: App) {
+    Object.keys(components).forEach(key => {
+        const component = components[key];
+        if (component.install) {
+            app.use(component);
+        }
+    });
+    return app;
+};
+
+export default {
+    install,
+};
