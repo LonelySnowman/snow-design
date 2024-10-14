73ca673cc441054535f5dcf6143f96677998a85a
LonelySnowman <2438490168@qq.com>
2024-10-15 01:42:20 +0800

    feat: add style lint config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     '*.{ts,tsx,js,jsx,css,mjs,json}': ['eslint --fix'],
     '*.{md,yml}': ['prettier --ignore-unknown --write'],
+    '*.{css,scss}': ['stylelint --fix'],
 };
---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-recommended-scss',
+        'stylelint-config-recess-order',
+    ],
+    overrides: [
+        {
+            files: ['**/*.(scss|css)'],
+            customSyntax: 'postcss-scss',
+        },
+    ],
+    ignoreFiles: ['**/*.{js,jsx,tsx,ts,json,md,yaml}'],
+    rules: {
+        'property-no-unknown': null,
+        'selector-class-pattern': null,
+        'property-no-vendor-prefix': null,
+        'color-hex-length': null,
+        'import-notation': null,
+        'block-no-empty': null, // @todo: 优化 _base 中需要的占位符
+    },
+};
