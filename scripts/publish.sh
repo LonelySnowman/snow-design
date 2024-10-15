45a21212a061b27720f3e307edd553f57b81ba26
LonelySnowman <2438490168@qq.com>
2024-10-15 22:58:55 +0800

    release: 0.0.25

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,31 +3,31 @@
 set -e
 
 cd packages/components
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/vue3
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/foundation
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/locale
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/theme-default
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/vite-plugin
-npm publish --access public
+pnpm publish --access public
 cd -
 
 cd packages/webpack-plugin
-npm publish --access public
+pnpm publish --access public
 cd -
 
 echo "✅ Publish completed"
